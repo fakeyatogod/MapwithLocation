2da4f5397c65f04543899b7e5852a74f017da15f
fakeyatogod <fakeyatokami@gmail.com>
2022-07-21 23:09:45 +0530

    Add destination logic, kind of

--- app/src/main/java/com/app/directionwithlocation/MainActivity.kt
+++ app/src/main/java/com/app/directionwithlocation/MainActivity.kt
@@ -3,19 +3,20 @@ package com.app.directionwithlocation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.app.directionwithlocation.ui.theme.DirectionWithLocationTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,26 +37,73 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MapScreen() {
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-    }
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-        Marker(
-            state = MarkerState(position = singapore),
-            title = "Singapore",
-            snippet = "Marker in Singapore"
-        )
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DirectionWithLocationTheme {
-        MapScreen()
+    var showMarker by remember {
+        mutableStateOf(false)
+    }
+    var destinationLocation by remember {
+        mutableStateOf( LatLng(1.35, 103.87))}
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(destinationLocation, 14f)
+    }
+    var isMapLoaded by remember { mutableStateOf(false) }
+
+    val uiSettings by remember { mutableStateOf(MapUiSettings()) }
+    var properties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.NORMAL))
+    }
+
+    Box(Modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier.matchParentSize(),
+            properties = properties,
+            uiSettings = uiSettings,
+            cameraPositionState = cameraPositionState,
+            onMapLoaded = {
+                isMapLoaded = true
+            } ,
+            onMapClick = {
+                destinationLocation = it
+                showMarker = true
+            }
+        ) {
+            if(showMarker) {
+                Marker(
+                    state = MarkerState(position = destinationLocation),
+                    title = "Destination",
+                    snippet = "${destinationLocation.latitude}, ${destinationLocation.longitude}"
+                )
+            }
+        }
+
+        // Buttons
+        Column(modifier = Modifier.fillMaxWidth().padding(all = 8.dp), horizontalAlignment = Alignment.End) {
+            Button(onClick = {
+                properties = if (properties == properties.copy(mapType = MapType.NORMAL)) {
+                    properties.copy(mapType = MapType.SATELLITE)
+                } else {
+                    properties.copy(mapType = MapType.NORMAL)
+                }
+            }
+            ) {
+                Text(text = "Toggle SATELLITE")
+            }
+        }
+
+        if (!isMapLoaded) {
+            AnimatedVisibility(
+                modifier = Modifier
+                    .matchParentSize(),
+                visible = !isMapLoaded,
+                enter = EnterTransition.None,
+                exit = fadeOut()
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.background)
+                        .wrapContentSize()
+                )
+            }
+        }
     }
 }
